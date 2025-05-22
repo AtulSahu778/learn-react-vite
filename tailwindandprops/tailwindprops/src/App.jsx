9ff39a25c27ba3252b196e4ea48d91870af8ece6
AtulSahu778 <sahuatul2005@gmail.com>
2025-05-22 23:36:51 +0530

    learnt how to connect tailwind css and use it

---
+++ tailwindandprops/tailwindprops/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h1 className='text-3xl bg-blue-900 rounded-xl p-4 font-bold underline'>Atul Sahu</h1>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
