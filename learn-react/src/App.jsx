57e3d76994bb8c186e6f58e95c12718a5e59050a
AtulSahu778 <sahuatul2005@gmail.com>
2025-05-22 23:00:42 +0530

    learnt about counters and hooks

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -1,33 +1,42 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [count, setCount] = useState(0)
+
+  const addValue = () => {
+    if(count >= 20){
+      count = 20
+    }
+    else{
+      setCount(count + 1)
+    }
+    
+  }
+  const removeValue = () => {
+    if(count <= 0){
+      count = 0
+    }
+    else{
+      setCount(count - 1)
+    }
+
+    
+  }
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Counter: {count}</h1>
+        <button
+        onClick={addValue}
+        >Add Value</button>
+        <button
+        onClick={removeValue}
+        >Remove Value</button>
+        <h2>Notification: {count}</h2>
+        <p>State: {count}</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
